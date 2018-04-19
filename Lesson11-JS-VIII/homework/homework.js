f462fadeffc98405e2e2391fddd93fb325e2fa86
jhain2 <38103175+jhain2@users.noreply.github.com>
2018-04-19 12:03:19 -0400

    Homework 11
    Homework11

--- Lesson11-JS-VIII/homework/homework.js
+++ Lesson11-JS-VIII/homework/homework.js
@@ -2,15 +2,39 @@
 
 // solve these recursively
 
-function nFactorial(n) {
+function nFactorial(n, totalSoFar) {
   // return the factorial for n
   // example:
   // the factorial of 3 is 6 (3 * 2 * 1)
+   totalSoFar = totalSoFar || 1;
+ /*if (totalSoFar === undefined) {
+    totalSoFar = 1;
+  }*/
+  
+  if (n < 1) {
+    return totalSoFar;
+  }
+  totalSoFar *= n;
+//totalSoFar = totalSoFar * n;
+  return nFactorial(n-1, totalSoFar);
+
 }
+
 
 function nFibonacci(n) {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+let a = 1, b = 0, temp;
+
+while (n >= 1){
+
+temp = a;
+
+a = a + b;
+b = temp;
+n--;
+}
+return a;
 }
 
 // Do not modify code below this line.
